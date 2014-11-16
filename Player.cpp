f20c4403523a598837da2ce9d250b22c9629c438
Andrew Kelley <atkelley@umass.edu>
2014-11-15 21:52:20 -0500

    Playerhaters.cpp

--- Player.cpp
+++ Player.cpp
@@ -1,7 +1,12 @@
 #include "Player.h"
 #include "Card.h"
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sstream>
 #include <string>
 #include <vector>
+
 using namespace std;
 Player::Player(bool h, string n){
     points = 0;
@@ -50,3 +55,49 @@ std::vector<Card*> Player::getScoreHand(){
 int Player::getPosition(){
     return position;
 }
+
+string Player::serialize(){
+ 	ostringstream stringStreamVal;
+	stringStreamVal << "var1" << position << "; var2=" << points << ";var3=" << isHuman << ";";
+	return stringStreamVal.str();
+}
+
+void Player::reset(string serial){
+	string delimiter = ";";	  
+        string delimiter2 = "=";
+	string token, token2, token3;
+	int position = 0, position2 = 0;
+        
+	while ((position = serial.find(delimiter)) != -1) {
+        	token = serial.substr(0, position);
+            	position2 = token.find(delimiter2);
+            	token2 = token.substr(0, position2);
+	    	token3 = token.substr(position2+1,token.size());
+	    	if (token2 == "position") {
+			position = atoi(token3.c_str());
+		}
+ 
+		else if (token2 == "points") {
+			points = atoi(token3.c_str());
+	    	}
+ 
+		else if (token2 == "isHuman") {
+			isHuman = atoi(token3.c_str());
+	    	} 
+
+		else {
+		cout << "error" << endl;
+	    	}
+            	
+		serial.erase(0, position + delimiter.length());
+	}
+}
+
+
+
+
+
+
+
+
+
